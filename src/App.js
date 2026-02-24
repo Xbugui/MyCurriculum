5ba65c1faf734ec687217136815834c74d4f223d
Xbugui <107008373+Xbugui@users.noreply.github.com>
2026-02-24 18:29:17 -0300

    Remove logo import and usage in App.js
    Removed logo import and related usage in App component.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import logo from './logo.svg';
 
 
 function App() {
-  return (
-    <img src={logo} className="App-logo" alt="logo" />
+  return (  
     <div className="App">
       <NavBar />
       <Banner />
