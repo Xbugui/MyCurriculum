35d6478758496fc092de4f79a6c0f2b811d9caa2
Xbugui <107008373+Xbugui@users.noreply.github.com>
2026-02-24 18:23:57 -0300

    Add logo image import and display in App component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import logo from './logo.svg';
+
 
 function App() {
   return (
+    <img src={logo} className="App-logo" alt="logo" />
     <div className="App">
       <NavBar />
       <Banner />
